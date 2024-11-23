2ea9ad9d8dd9b62aeb26313ee6a0aab13fa3f1c8
Svyshch bc2024-5 <den.svyshch@gmail.com>
2024-11-23 20:24:29 +0200

    Додано GET /notes/<iм'я нотатки>.

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const { Command } = require('commander');
+const fs = require('fs');
+const path = require('path');
+
 
 const app = express();
 const program = new Command();
@@ -13,11 +16,19 @@ program
 
 const options = program.opts();
 
+const cacheDir = path.resolve(options.cache);
+if (!fs.existsSync(cacheDir)) {
+  fs.mkdirSync(cacheDir, { recursive: true });
+}
 
-// Налаштування сервера
-app.get('/', (req, res) => {
-  res.send('Hello! you see me?');
-});
+app.get('/notes/:noteName', (req, res) => {
+    const notePath = path.join(cacheDir, req.params.noteName + '.txt');
+    if (!fs.existsSync(notePath)) {
+      return res.status(404).send('Not found');
+    }
+    const noteContent = fs.readFileSync(notePath, 'utf-8');
+    res.send(noteContent);
+  });
 
 // Запуск сервера
 app.listen(options.port, options.host, () => {
