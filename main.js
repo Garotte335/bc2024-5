9a04be98005f521819bd028d31ce923438596edf
Svyshch bc2024-5 <den.svyshch@gmail.com>
2024-11-23 20:30:09 +0200

    Виконано Частину 2

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Command } = require('commander');
 const fs = require('fs');
 const path = require('path');
-
+const multer = require('multer');
 
 const app = express();
 const program = new Command();
@@ -20,6 +20,9 @@ const cacheDir = path.resolve(options.cache);
 if (!fs.existsSync(cacheDir)) {
   fs.mkdirSync(cacheDir, { recursive: true });
 }
+
+app.use(express.json());
+const upload = multer();
 
 app.get('/notes/:noteName', (req, res) => {
     const notePath = path.join(cacheDir, req.params.noteName + '.txt');
@@ -48,6 +51,30 @@ app.get('/notes/:noteName', (req, res) => {
     res.send('Note deleted');
   });
 
+  app.get('/notes', (req, res) => {
+    const notes = fs.readdirSync(cacheDir).map(file => {
+      const noteName = path.parse(file).name;
+      const noteText = fs.readFileSync(path.join(cacheDir, file), 'utf-8');
+      return { name: noteName, text: noteText };
+    });
+    res.json(notes);
+  });
+
+  app.post('/write', upload.none(), (req, res) => {
+    const { note_name, note } = req.body;
+    const notePath = path.join(cacheDir, note_name + '.txt');
+    
+    if (fs.existsSync(notePath)) {
+      return res.status(400).send('Note already exists');
+    }
+    
+    fs.writeFileSync(notePath, note);
+    res.status(201).send('Note created');
+  });
+  
+  app.get('/UploadForm.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'UploadForm.html'));
+  });
 
 // Запуск сервера
 app.listen(options.port, options.host, () => {
